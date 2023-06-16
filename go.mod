414e40397e46e46d044e2ddefbc5ab42091e8466
Muhammad Naufal <mnaufal0999@gmail.com>
2023-06-16 14:23:21 +0700

    update module from my helper module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/emnopal/simple_go_rest_api_service
 
 go 1.20
+
+require github.com/emnopal/go_helper v0.0.1 // indirect
--- main.go
+++ main.go
@@ -4,16 +4,9 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
-)
 
-func GetENV(key string, default_value string) (ENV_VALUE string) {
-	ENV_VALUE = os.Getenv(key)
-	if ENV_VALUE == "" {
-		ENV_VALUE = default_value
-	}
-	return
-}
+	helper "github.com/emnopal/go_helper"
+)
 
 func main() {
 
@@ -25,7 +18,7 @@ func main() {
 
 	log.Println("Listing for requests at http://localhost:8000/hello")
 
-	PORTS := GetENV("PORTS", ":8000")
+	PORTS := helper.GetENV("PORTS", ":8000")
 
 	log.Fatal(http.ListenAndServe(PORTS, nil))
 
